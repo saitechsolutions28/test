6d5c50a8f5d3e3352e6d112e73a995992e4fbfe8
saitechsolutions28 <saitechsolutions28@gmail.com>
2026-08-07 14:20:08 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,23 +5,15 @@ require("dotenv").config();
 
 const app = express();
 
-DB_HOST=db.gefvwnefgnarsrkzeicc.supabase.co
-DB_PORT=5432
-DB_NAME=postgres
-DB_USER=postgres
-DB_PASSWORD=TRXizw3wERx2Yk1P
-
-PORT=5000
-
 app.use(cors());
 app.use(express.json());
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  host: "db.gefvwnefgnarsrkzeicc.supabase.co",
+  port: 5432,
+  database: "postgres",
+  user: "postgres",
+  password: "TRXizw3wERx2Yk1P",
   ssl: {
     rejectUnauthorized: false,
   },
